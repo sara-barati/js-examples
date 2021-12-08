36dc2c8699091a3ffd8ce6fc5f982709e6d8163c
sara <sarabarati045@gmail.com>
2021-12-08 23:14:18 +0330

    soale2-bigint

--- soal2/main.js
+++ soal2/main.js
@@ -5,26 +5,29 @@ const x={
         }
         this.maxnum=BigInt(Number.MIN_SAFE_INTEGER)
         for(let org of arguments){
-         if(typeof org!=="bigint"){
-          if(org>Number.MAX_SAFE_INTEGER||org%1!==0||org==""){
+          if(org>Number.MAX_SAFE_INTEGER||org===""){
+              return"BIG ERROR"
+          }
+          if(typeof org!=="bigint"&&org%1!==0){
               return"BIG ERROR"
           }
           else{
               org=BigInt(org)
-          }}
+          }
          if(org>this.maxnum)
           {
              this.maxnum=org
           }}
-            return this.maxnum
+            return (this.maxnum)
         }  
 }
 console.log(x.max(1n,10n,5n));
 console.log(x.max("10",5n,1));
 console.log(x.max(0n));
 console.log(x.max(2**53-1));
-console.log(x.max(2**53));
+console.log(x.max(5.5));
 console.log(x.max("10"/3));
 console.log(x.max("10"/2));
-console.log(x.max(10 - 12, 10 - 10, 10 - 11))
+console.log(x.max(10 - 12, 10 - 10, 10 - 11));
+
 
